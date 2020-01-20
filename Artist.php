f98c3102ab30ce2fdde4ca36ec4d6b3ce2a4a43e
Nicolas Hungerbühler <31136697+Grimlix@users.noreply.github.com>
2020-01-21 00:42:01 +0100

    Update Artist.php

--- Artist.php
+++ Artist.php
@@ -2,38 +2,39 @@
 require_once "connexion bbd.php";
 ?>
 
-
-
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <title>Frontpage</title>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="loginStyle.css">
 </head>
-
-    
-    
-    
-<body>  
-<h1>List of artist:</h1>
+<body>
+ 
 <?php
+    $reponse = $bdd->query("SELECT Person.id, firstname, lastname, stagename, birthdate, biography FROM Person INNER JOIN Artist ON Person.id = Artist.id INNER JOIN Solo ON Solo.id = Person.id WHERE Person.id=" . $_GET['id']);
+    $ligne = $reponse->fetch();
+    ?>
 
-    $reponse = $bdd->query("SELECT stageName, id FROM Artist");
+    <h1> <?= $ligne['stagename'] ?> </h1>
+    <h1>Informations: </h1>
+    <p> Firstname: <?= $ligne["firstname"] ?> </p>
+    <p> Lastname: <?= $ligne["lastname"] ?> </p>
+    <p> Birthday: <?= $ligne["birthdate"] ?> </p>
+    
+    <h1>Biography:</h1>
+    <?= $ligne['biography'] ?>
+    
+    <h1>Ses sons:</h1>
+<?php
+     
+    $reponse = $bdd->query("SELECT Music.id, title FROM Music INNER JOIN Artist ON Artist.id = Music.id WHERE Music.idArtist = " . $_GET['id']);
     while($ligne = $reponse->fetch()){
         ?> 
-        <a href="Music_Person.php?id=<?= $ligne["id"] ?> "><p> <?= $ligne["stageName"]?> </p>
-    </a>
-    
-    
-    
+    <a href="Tracks.php?id=<?= $ligne["id"] ?>"><p><?= $ligne["title"]?></p></a>
         <?php 
     }
     ?>
+    
 
 </body>
 </html>

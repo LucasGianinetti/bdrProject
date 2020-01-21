0a42ba8ab88a221a928a6d777be0936b264389a9
LucasGianinetti <49147622+LucasGianinetti@users.noreply.github.com>
2020-01-21 13:18:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -39,6 +39,33 @@ session_start();
 
   <input type="submit" value="Search"><br>
 </form>
+
+<?php 
+  $reponse = $bdd->query("SELECT music.id, music.title FROM music INNER JOIN track_adaptation ON track_adaptation.idTrack = music.id
+    ORDER BY releaseDate DESC LIMIT 10");
+  ?>
+
+
+
+  <table data-toggle="table" class="bg-light text-dark">
+    <thead >
+        <th scope="col"> New musics ! :</th>
+    </thead>
+    <tbody>
+    <?php
+    while($ligne = $reponse->fetch()){
+    ?>
+    <tr><td>
+    <a href="Tracks.php?id=<?= $ligne["id"] ?>"><p><?= $ligne["title"]?></p></a></td></tr>
+        <?php 
+    }
+    ?>
+</tbody></table>
+    
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/bootstrap-table@1.15.5/dist/bootstrap-table.min.js"></script>
     
 </body>
 </html>

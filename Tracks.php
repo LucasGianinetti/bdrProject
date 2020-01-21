2cb6d38a3e5561c91d3a658be767799cb0d01c34
Nicolas Hungerbühler <31136697+Grimlix@users.noreply.github.com>
2020-01-21 15:45:51 +0100

    Update Tracks.php

--- Tracks.php
+++ Tracks.php
@@ -21,39 +21,45 @@ require_once "connexion bbd.php";
     <a href="index.php" class="btn btn-primary">Home</a>   
     
     
+    
 <?php
-    $reponse1 = $bdd->query("SELECT Adaptation.adaptationName FROM Track INNER JOIN Track_Adaptation ON Track.id = Track_Adaptation.idTrack INNER JOIN Adaptation ON Track_Adaptation.adaptationName = Adaptation.adaptationName WHERE Track.id = " . $_GET['id']);
-    $ligne1 = $reponse1->fetch();
+    $id = $_GET['id'];
+    $genre = $_GET['genre'];
+    $title = $_GET['title'];
+    $genre = $_GET['genre'];
+    $adaptation = $_GET['adaptation'];
     ?>
+    
 <?php
-    $reponse2 = $bdd->query("SELECT releaseDate, length, title FROM Track INNER JOIN Track_Adaptation ON Track.id = Track_Adaptation.idTrack INNER JOIN Music ON Music.id = Track.id WHERE Track.id = " . $_GET['id']);
+    $reponse2 = $bdd->query("SELECT releaseDate, length FROM Track INNER JOIN Track_Adaptation ON Track.id = Track_Adaptation.idTrack INNER JOIN Music ON Music.id = Track.id WHERE Track.id = " . $_GET['id']);
     $ligne2 = $reponse2->fetch();
     ?>
     <h1>Informations:</h1>
-    <p>Title: <?=$ligne2["title"]?></p>
-    <p>Adaptation: <?=$ligne1["adaptationName"]?></p>
+    <p>Title: <?= $title ?></p>
+    <p>Adaptation: <?=$adaptation?></p>
     <p>Release date : <?=$ligne2["releaseDate"]?></p>
-    <p>Durée: <?= $ligne2["length"] ?> </p>
+    <p>Length : <?= $ligne2["length"] ?> </p>
+    <p>Genre : <?= $genre ?></p>
     
     <h1>Recommendations:</h1>
 <?php
-    $reponse4 = $bdd->query("SELECT Track.id, Music.title FROM Music INNER JOIN Track ON Track.id = Music.id INNER JOIN Track_Adaptation ON Track:Aaptation.idTrack ");
-    while($ligne4 = $reponse4->fetch()){
-        ?>
-        <a href="Tracks.php?id=<?= $ligne4["id"] ?>"><p><?= $ligne4["title"]?> </p></a>
-        <?php
-    }
-    ?>
-    
-     
-<?php
-    $reponse3 = $bdd->query("SELECT Track.id, Music.title FROM Track INNER JOIN Music ON Music.id = Track.id INNER JOIN Track_Genre ON Track.id = Track_Genre.idTrack INNER JOIN Genre ON Track_Genre.idGenre = Genre.label INNER JOIN Track_Adaptation ON Track_Adaptation.idTrack = Track.id WHERE Track.id !=" . $_GET['id']);
+    $reponse3 = $bdd->query("SELECT Music.id, Music.title, adaptationName FROM Music INNER JOIN Track ON Track.id = Music.id INNER JOIN Track_Adaptation ON idTrack = Track.id WHERE Music.title = '$title' AND Track_Adaptation.adaptationName != '$adaptation'");
     while($ligne3 = $reponse3->fetch()){
         ?>
-        <a href="Tracks.php?id=<?= $ligne3["id"] ?>"><p><?= $ligne3["title"]?> </p></a>
-        <?php
+            <a href="Tracks.php?id=<?= $ligne3["id"] ?>"><p><?= $ligne3["adaptationName"]?></p></a>
+        <?php 
     }
     ?>
+
+<?php
+    $reponse4 = $bdd->query("SELECT Music.id, Music.title FROM Music INNER JOIN Track ON Track.id = Music.id INNER JOIN Track_Genre ON Track.id = Track_Genre.idTrack WHERE Music.title != '$title' AND Track_Genre.idGenre = '$genre' LIMIT 3");
+    while($ligne4 = $reponse4->fetch()){
+        ?>
+            <a href="Tracks.php?id=<?= $ligne4["id"] ?>"><p><?= $ligne4["title"]?></p></a>
+        <?php 
+    }
+    ?>
+
     
 <?php
     $reponse = $bdd->query("SELECT idTrack, link FROM LinkTrack INNER JOIN Track ON Track.id = idTrack WHERE idTrack =" . $_GET['id']);

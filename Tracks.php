96e18b3e845d085e59af8acc07c9a910848bcd94
Nicolas Hungerbühler <31136697+Grimlix@users.noreply.github.com>
2020-01-21 04:47:31 +0100

    Fonctionne vite fait

--- Tracks.php
+++ Tracks.php
@@ -1,44 +1,33 @@
 <?php
+error_reporting(0);
 require_once "connexion bbd.php";
 ?>
 
-
-
-
-
-
-
-
-
-
-    
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="loginStyle.css">
+</head>
+<body>
     
     
 <?php
     $reponse2 = $bdd->query("SELECT idTrack, link FROM LinkTrack INNER JOIN Track ON Track.id = idTrack WHERE idTrack = " . $_GET['id']);
-
     $ligne = $reponse2->fetch();
 ?>
     
-    <iframe src= "<?= $ligne["link"]?>" width="100%">
-    </iframe>
-
-    
-    
-    <?php
-    $reponse = $bdd->query("SELECT Track.id, Track.length FROM Track INNER JOIN Music ON Music.id = Track.id WHERE Track.id = " . $_GET['id']);
-    $ligne = $reponse->fetch();
+<?php
+    $reponse3 = $bdd->query("SELECT Track.id, length, releaseDate, Adaptation.adaptationName FROM Track INNER JOIN Track_Adaptation ON Track.id = Track_Adaptation.idTrack INNER JOIN Adaptation ON Track_Adaptation.adaptationName = Adaptation.adaptationName WHERE Track.id = " . $_GET['id']);
+    $ligne3 = $reponse3->fetch();
     ?>
-<p>Durée: <?= $ligne["length"] ?> </p> </a>
+    <h1>Informations:</h1>
+    <p>Adaptation: <?=$ligne3["adaptationName"]?></p>
+    <p>Release date : <?=$ligne3["releaseDate"]?></p>
+    <p>Durée: <?= $ligne3["length"] ?> </p>
     
-<span class="fa fa-star checked"></span>
-<span class="fa fa-star checked"></span>
-<span class="fa fa-star checked"></span>
-<span class="fa fa-star"></span>
-<span class="fa fa-star"></span>
     
 
     
 </body>
-
 </html>

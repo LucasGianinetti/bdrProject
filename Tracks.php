7ed1e82b4194bd6a42fed4d8122de46fd99d15be
Nicolas Hungerbühler <31136697+Grimlix@users.noreply.github.com>
2020-01-21 17:33:42 +0100

    Update Tracks.php

--- Tracks.php
+++ Tracks.php
@@ -1,3 +1,4 @@
+
 <?php
 error_reporting(0);
 require_once "connexion bbd.php";
@@ -20,8 +21,6 @@ require_once "connexion bbd.php";
     <a href="Account.php" class="btn btn-primary">Account info</a> 
     <a href="index.php" class="btn btn-primary">Home</a>   
     
-    
-    
 <?php
     $id = $_GET['id'];
     $genre = $_GET['genre'];
@@ -30,8 +29,10 @@ require_once "connexion bbd.php";
     $adaptation = $_GET['adaptation'];
     ?>
     
+    <h1><?=$title?></h1>
+    
 <?php
-    $reponse2 = $bdd->query("SELECT releaseDate, length FROM Track INNER JOIN Track_Adaptation ON Track.id = Track_Adaptation.idTrack INNER JOIN Music ON Music.id = Track.id WHERE Track.id = " . $_GET['id']);
+    $reponse2 = $bdd->query("SELECT releaseDate, length, stagename, Artist.id FROM Track INNER JOIN Track_Adaptation ON Track.id = Track_Adaptation.idTrack INNER JOIN Music ON Music.id = Track.id INNER JOIN Artist ON Artist.id = Music.idArtist WHERE Track.id = " . $_GET['id']);
     $ligne2 = $reponse2->fetch();
     ?>
     <h1>Informations:</h1>
@@ -39,6 +40,7 @@ require_once "connexion bbd.php";
     <p>Adaptation: <?=$adaptation?></p>
     <p>Release date : <?=$ligne2["releaseDate"]?></p>
     <p>Length : <?= $ligne2["length"] ?> </p>
+    <p>Artist : <a href="Artist.php?id=<?= $ligne2["id"] ?>"><?= $ligne2["stagename"]?></a>
     <p>Genre : <?= $genre ?></p>
     
     <h1>Recommendations:</h1>
@@ -52,7 +54,7 @@ require_once "connexion bbd.php";
     ?>
 
 <?php
-    $reponse4 = $bdd->query("SELECT Music.id, title, adaptationName, Track_Genre.idGenre  FROM Music INNER JOIN Track ON Track.id = Music.id INNER JOIN Track_Adaptation ON Track_Adaptation.idTrack = Track.id  INNER JOIN Track_Genre ON Track.id = Track_Genre.idTrack WHERE Music.title != '$title' AND Track_Genre.idGenre = '$genre' LIMIT 4");
+    $reponse4 = $bdd->query("SELECT Music.id, title, adaptationName, Track_Genre.idGenre  FROM Music INNER JOIN Track ON Track.id = Music.id INNER JOIN Track_Adaptation ON Track_Adaptation.idTrack = Track.id  INNER JOIN Track_Genre ON Track.id = Track_Genre.idTrack WHERE Music.title != '$title' AND Track_Genre.idGenre = '$genre' ORDER BY rand() LIMIT 4");
     while($ligne4 = $reponse4->fetch()){
         ?>
            <a href="Tracks.php?id=<?= $ligne4["id"] ?>&title=<?= $ligne4["title"]?>&adaptation=<?= $ligne4["adaptationName"]?>&genre=<?= $ligne4["idGenre"]?>"><p><?= $ligne4["title"]?>

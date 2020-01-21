91313233a7369a6db826713ed71d5f32f55886ef
LucasGianinetti <49147622+LucasGianinetti@users.noreply.github.com>
2020-01-21 10:38:48 +0100

    Update Artist.php

--- Artist.php
+++ Artist.php
@@ -5,10 +5,11 @@ require_once "connexion bbd.php";
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="loginStyle.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.15.5/dist/bootstrap-table.min.css">
 </head>
-<body>
+<body class="bg-info">
  
 <?php
     $reponse = $bdd->query("SELECT Person.id, firstname, lastname, stagename, birthdate, biography FROM Person INNER JOIN Artist ON Person.id = Artist.id INNER JOIN Solo ON Solo.id = Person.id WHERE Person.id=" . $_GET['id']);
@@ -26,23 +27,43 @@ require_once "connexion bbd.php";
     
     <h1>Songs:</h1>
 <?php
-    $reponse = $bdd->query("SELECT Music.id, title FROM Music INNER JOIN Artist ON Artist.id = Music.id INNER JOIN Track ON Track.id = Music.id INNER JOIN Track_Adaptation ON Track.id = Track_Adaptation.idTrack WHERE Music.idArtist = " . $_GET['id']);
+    $reponse = $bdd->query("SELECT Music.id, title FROM Music INNER JOIN Artist ON Artist.id = Music.idArtist INNER JOIN Track ON Track.id = Music.id INNER JOIN Track_Adaptation ON Track.id = Track_Adaptation.idTrack WHERE Music.idArtist = " . $_GET['id']);
+    ?>
+     <table data-toggle="table" class="bg-light text-dark">
+    <thead >
+        <th scope="col"> Musics: </th>
+    </thead>
+    <tbody>
+    <?php
     while($ligne = $reponse->fetch()){
-        ?> 
-    <a href="Tracks.php?id=<?= $ligne["id"] ?>"><p><?= $ligne["title"]?></p></a>
+    ?>
+    <tr><td>
+    <a href="Tracks.php?id=<?= $ligne["id"] ?>"><p><?= $ligne["title"]?></p></a></td></tr>
         <?php 
     }
     ?>
+</tbody></table>
     <h1>Albums:</h1>
 <?php
-    $reponse2 = $bdd->query("SELECT Music.id, title FROM Music INNER JOIN Artist ON Artist.id = Music.id INNER JOIN Album ON Album.id = Music.id WHERE Music.idArtist = " . $_GET['id']);
+    $reponse2 = $bdd->query("SELECT Music.id, title FROM Music INNER JOIN Artist ON Artist.id = Music.idArtist INNER JOIN Album ON Album.id = Music.id WHERE Music.idArtist = " . $_GET['id']);
+    ?>
+     <table data-toggle="table" class="bg-light text-dark">
+    <thead >
+        <th scope="col"> Albums : </th>
+    </thead>
+    <tbody>
+    <?php
     while($ligne2 = $reponse2->fetch()){
-        ?> 
-    <a href="Album.php?id=<?= $ligne2["id"] ?>"><p><?= $ligne2["title"]?></p></a>
+    ?>
+    <tr><td>
+    <a href="Album.php?id=<?= $ligne2["id"] ?>"><p><?= $ligne2["title"]?></p></a></td></tr>
         <?php 
     }
     ?>
-     
-
+     </tbody></table>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/bootstrap-table@1.15.5/dist/bootstrap-table.min.js"></script>
 </body>
 </html>

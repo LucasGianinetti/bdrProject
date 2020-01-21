f58e667dd906e54a399c9f07c9e420ce25085e7b
Nicolas Hungerbühler <31136697+Grimlix@users.noreply.github.com>
2020-01-21 04:48:51 +0100

    Fonctionne, ajouter Band/Solo

--- Artist.php
+++ Artist.php
@@ -24,17 +24,25 @@ require_once "connexion bbd.php";
     <h1>Biography:</h1>
     <?= $ligne['biography'] ?>
     
-    <h1>Ses sons:</h1>
+    <h1>Songs:</h1>
 <?php
-     
-    $reponse = $bdd->query("SELECT Music.id, title FROM Music INNER JOIN Artist ON Artist.id = Music.id WHERE Music.idArtist = " . $_GET['id']);
+    $reponse = $bdd->query("SELECT Music.id, title FROM Music INNER JOIN Artist ON Artist.id = Music.id INNER JOIN Track ON Track.id = Music.id INNER JOIN Track_Adaptation ON Track.id = Track_Adaptation.idTrack WHERE Music.idArtist = " . $_GET['id']);
     while($ligne = $reponse->fetch()){
         ?> 
     <a href="Tracks.php?id=<?= $ligne["id"] ?>"><p><?= $ligne["title"]?></p></a>
         <?php 
     }
     ?>
-    
+    <h1>Albums:</h1>
+<?php
+    $reponse2 = $bdd->query("SELECT Music.id, title FROM Music INNER JOIN Artist ON Artist.id = Music.id INNER JOIN Album ON Album.id = Music.id WHERE Music.idArtist = " . $_GET['id']);
+    while($ligne2 = $reponse2->fetch()){
+        ?> 
+    <a href="Album.php?id=<?= $ligne2["id"] ?>"><p><?= $ligne2["title"]?></p></a>
+        <?php 
+    }
+    ?>
+     
 
 </body>
 </html>

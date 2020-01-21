7354eea631016da35479444c551bcb2455a3f62a
Nicolas Hungerbühler <31136697+Grimlix@users.noreply.github.com>
2020-01-21 22:42:26 +0100

    Update script.sql

--- script.sql
+++ script.sql
@@ -8,12 +8,12 @@ CREATE TABLE Person(
 	id INT(11) PRIMARY KEY AUTO_INCREMENT,
     lastname VARCHAR(30),
     firstname VARCHAR(30),
-    birthdate DATE
+    birthdate DATE NOT NULL
 );
 
 CREATE TABLE Customer(
 	id INT(11) PRIMARY KEY,
-    username VARCHAR(30) UNIQUE,
+    username VARCHAR(30) NOT NULL UNIQUE,
     pwd VARCHAR(30) NOT NULL,
 	FOREIGN KEY(id) REFERENCES Person(id)
 		ON UPDATE CASCADE
@@ -38,25 +38,30 @@ CREATE TABLE Solo(
 
 CREATE TABLE Band(
 	id INT(11) PRIMARY KEY,
-    bandName VARCHAR(20),
+    bandname VARCHAR(20) NOT NULL,
 	FOREIGN KEY(id) REFERENCES Person(id)
 		ON UPDATE CASCADE
 		ON DELETE CASCADE
 );
 
 CREATE TABLE Band_Solo(
-	id INT(11) PRIMARY KEY,
-	dateEntry DATE,
-    dateExit DATE,
-    FOREIGN KEY(id) REFERENCES Person(id)
+	idSolo INT(11),
+    idBand INT(11),
+	dateEntry DATE NOT NULL,
+    dateExit DATE NOT NULL,
+    FOREIGN KEY(idSolo) REFERENCES Person(id)
 		ON UPDATE CASCADE
-        ON DELETE CASCADE
+        ON DELETE CASCADE,
+	FOREIGN KEY(idBand) REFERENCES Person(id)
+		ON UPDATE CASCADE
+        ON DELETE CASCADE,
+	PRIMARY KEY(idSolo, idBand) 
 );
 
 CREATE TABLE Music(
 	id INT(11) PRIMARY KEY AUTO_INCREMENT,
-    idArtist INT(11),
-	title VARCHAR(100),
+    idArtist INT(11) NOT NULL,
+	title VARCHAR(100) NOT NULL,
 	FOREIGN KEY(idArtist) REFERENCES Person(id)
 		ON UPDATE CASCADE
         ON DELETE CASCADE
@@ -76,7 +81,7 @@ CREATE TABLE Customer_Music(
 
 CREATE TABLE Album(
 	id INT(11) PRIMARY KEY,
-	releaseDate DATE,
+	releaseDate DATE NOT NULL,
     FOREIGN KEY(id) REFERENCES Music(id)
 		ON UPDATE CASCADE
         ON DELETE CASCADE
@@ -85,6 +90,7 @@ CREATE TABLE Album(
 CREATE TABLE Customer_Album(
 	idUser INT(11),
 	idAlbum INT(11),
+	grade INT(1) NOT NULL,
     FOREIGN KEY(idUser) REFERENCES Customer(id)
 		ON UPDATE CASCADE
         ON DELETE CASCADE,
@@ -96,7 +102,7 @@ CREATE TABLE Customer_Album(
 
 CREATE TABLE Track(
 	id INT(11) PRIMARY KEY,
-    length TIME,
+    length TIME NOT NULL,
     FOREIGN KEY (id) REFERENCES Music(id)
 		ON UPDATE CASCADE
         ON DELETE CASCADE
@@ -107,27 +113,27 @@ CREATE TABLE Genre(
 );
 
 CREATE TABLE Adaptation(
-	adaptationName VARCHAR(50) PRIMARY KEY
+	adaptationName VARCHAR(20) PRIMARY KEY
 );
 
 CREATE TABLE Track_Genre(
 	idTrack INT(11),
-    idGenre VARCHAR(20),
+    labelGenre VARCHAR(20),
 	FOREIGN KEY(idTrack) REFERENCES Music(id)
 		ON UPDATE CASCADE
         ON DELETE CASCADE,
-	FOREIGN KEY(idGenre) REFERENCES Genre(label)
+	FOREIGN KEY(labelGenre) REFERENCES Genre(label)
 		ON UPDATE CASCADE
         ON DELETE CASCADE,
-	PRIMARY KEY(idTrack, idGenre)
+	PRIMARY KEY(idTrack, labelGenre)
 );
 
 CREATE TABLE Track_Adaptation(
 	id INT(11) PRIMARY KEY AUTO_INCREMENT,
     idTrack INT(11) NOT NULL,
     idAlbum INT(11),
-    adaptationName VARCHAR(50) NOT NULL DEFAULT 'Original',
-    releaseDate DATE,
+    adaptationName VARCHAR(20) NOT NULL DEFAULT 'Original',
+    releaseDate DATE NOT NULL,
     FOREIGN KEY(idTrack) REFERENCES Track(id)
 		ON UPDATE CASCADE
         ON DELETE CASCADE,
@@ -160,6 +166,41 @@ CREATE TABLE LinkTrack(
 		ON UPDATE CASCADE
         ON DELETE CASCADE
 );
+
+DELIMITER $$
+CREATE TRIGGER check_releaseDate_1
+BEFORE INSERT ON Album
+FOR EACH ROW
+BEGIN
+	IF(NEW.releaseDate > CURRENT_DATE())
+    THEN SIGNAL SQLSTATE '45000'
+		SET message_text = 'Error, releaseDate is wrong.';
+	END IF;
+END
+$$
+
+DELIMITER $$
+CREATE TRIGGER check_releaseDate_2
+BEFORE INSERT ON Track_Adaptation
+FOR EACH ROW
+BEGIN
+	IF(NEW.releaseDate > CURRENT_DATE())
+    THEN SIGNAL SQLSTATE '45000'
+		SET message_text = 'Error, releaseDate is wrong.';
+	END IF;
+END
+$$
+
+DELIMITER $$
+CREATE TRIGGER date_check BEFORE INSERT ON Band_Solo
+FOR EACH ROW
+BEGIN
+  IF NEW.dateEntry > New.dateExit
+    THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Error, dateEntry cannot be after dateExit';
+  END IF;
+END
+$$
+
 
 INSERT INTO Person (lastname, firstname, birthdate)
 VALUES ("Keen", "Will", '1994.02.26'), ("Van der Schoot", "Mark", "1990-10-04"),
@@ -198,18 +239,12 @@ With over twelve years spent in choirs, orchestras and grade eight achievements 
  it was inevitable that music was going to play a prominent role in Will’s future. 
  After discovering a love for Pendulum and becoming inspired to delve into the world of music production, Whiney had his debut release on Subsphere Recordings back in 2012,
  an exquisite collaboration with Keeno called the “Sweetest Sin” EP.");
- 
-INSERT INTO Band
-VALUES (3, "Fred V & Grafix"), (4, "Fred V & Grafix");
-
-INSERT INTO Band_Solo (id, dateEntry)
-VALUES (3, '2009.01.01'), (4, '2009.01.01');
 
 INSERT INTO Music (idArtist, title)
 VALUES (5, "Beggin by letting go"), (1, "Piano only"), (5, "In Stillness"), (6, "Beggin by letting go"), (5,"A Hundred Oceans"), (5, "Fire Lit Sky"), (5, "Bears Breeches");
 
 INSERT INTO Album
-VALUES (3, '2015.01.01');
+VALUES (3, '2017.01.01');
 
 INSERT INTO Track 
 VALUES (1, '2:43'), (2, '3:25'), (4, '3:43'), (5, '3:21'), (6, '3:21'), (7, '2:50');
@@ -224,7 +259,7 @@ INSERT INTO Track_Genre
 VALUES (1, "Drum and bass"), (2, "Drum and bass"), (4, "Drum and bass"), (5, "Drum and bass"), (6, "Drum and bass"), (7, "Drum and bass");
 
 INSERT INTO Track_Adaptation(idTrack,adaptationName, releaseDate)
-VALUES (4,"Remix", "2019.01.01");
+VALUES (4,"Remix", "2019.01.12");
 INSERT INTO Track_Adaptation(idTrack, releaseDate)
 VALUES (1, "2019.01.01"), (2,"2018.12.12");
 INSERT INTO Track_Adaptation(idTrack, idAlbum, releaseDate)

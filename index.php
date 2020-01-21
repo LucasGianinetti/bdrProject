ff1a2181e032671dbb1f9b44dfd23fad9f8c49d3
Nicolas Hungerbühler <31136697+Grimlix@users.noreply.github.com>
2020-01-21 17:04:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -42,8 +42,6 @@ session_start();
   $reponse = $bdd->query("SELECT Music.id, title, adaptationName, Track_Genre.idGenre  FROM Music INNER JOIN Track ON Track.id = Music.id INNER JOIN Track_Adaptation ON Track_Adaptation.idTrack = Track.id  INNER JOIN Track_Genre ON Track.id = Track_Genre.idTrack ORDER BY releaseDate DESC LIMIT 10");
   ?>
 
-
-
   <table data-toggle="table" class="bg-light text-dark">
     <thead >
         <th scope="col"> New musics ! :</th>
@@ -53,13 +51,23 @@ session_start();
     while($ligne = $reponse->fetch()){
     ?>
     <tr><td>
-    <a href="Tracks.php?id=<?= $ligne["id"] ?>&title=<?= $ligne["title"]?>&adaptation=<?= $ligne["adaptationName"]?>&genre=<?= $ligne["idGenre"]?>"><p><?= $ligne["title"]?></p></a></td></tr>
+    <a href="Tracks.php?id=<?= $ligne["id"] ?>&title=<?= $ligne["title"]?>&adaptation=<?= $ligne["adaptationName"]?>&genre=<?= $ligne["idGenre"]?>"><p><?= $ligne["title"]?>
+        
+        <?php
+        if($ligne["adaptationName"] != "Original"){
+            ?>
+                (<?=$ligne["adaptationName"]?>)
+        <?php
+            } 
+        ?>
+        
+        </p></a></td></tr>
         <?php 
     }
     ?>
 </tbody></table>
     
-        <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/bootstrap-table@1.15.5/dist/bootstrap-table.min.js"></script>
